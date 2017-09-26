27298d01337fde9c7435bcb6c21b923f2ceae110
Dania luis <css.luism@gmail.com>
2017-09-26 16:52:52 +0000

    Initial commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,292 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.zmmtransport.controller.Main", {
+		handleRouteMatched: function(oEvent) {
+
+			var oParams = {};
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+				var oPath;
+				if (this.sContext) {
+					oPath = {
+						path: "/" + this.sContext,
+						parameters: oParams
+					};
+					this.getView().bindObject(oPath);
+				}
+			}
+
+		},
+		_onTableItemPress: function(oEvent) {
+
+			var oBindingContext = oEvent.getParameter("listItem").getBindingContext();
+
+			return new Promise(function(fnResolve) {
+				this.doNavigate("Detail", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		doNavigate: function(sRouteName, oBindingContext, fnPromiseResolve, sViaRelation) {
+
+			var sPath = (oBindingContext) ? oBindingContext.getPath() : null;
+			var oModel = (oBindingContext) ? oBindingContext.getModel() : null;
+
+			var sEntityNameSet;
+			if (sPath !== null && sPath !== "") {
+				if (sPath.substring(0, 1) === "/") {
+					sPath = sPath.substring(1);
+				}
+				sEntityNameSet = sPath.split("(")[0];
+			}
+			var sNavigationPropertyName;
+			var sMasterContext = this.sMasterContext ? this.sMasterContext : sPath;
+
+			if (sEntityNameSet !== null) {
+				sNavigationPropertyName = sViaRelation || this.getOwnerComponent().getNavigationPropertyForNavigationWithContext(sEntityNameSet,
+					sRouteName);
+			}
+			if (sNavigationPropertyName !== null && sNavigationPropertyName !== undefined) {
+				if (sNavigationPropertyName === "") {
+					this.oRouter.navTo(sRouteName, {
+						context: sPath,
+						masterContext: sMasterContext
+					}, false);
+				} else {
+					oModel.createBindingContext(sNavigationPropertyName, oBindingContext, null, function(bindingContext) {
+						if (bindingContext) {
+							sPath = bindingContext.getPath();
+							if (sPath.substring(0, 1) === "/") {
+								sPath = sPath.substring(1);
+							}
+						} else {
+							sPath = "undefined";
+						}
+
+						// If the navigation is a 1-n, sPath would be "undefined" as this is not supported in Build
+						if (sPath === "undefined") {
+							this.oRouter.navTo(sRouteName);
+						} else {
+							this.oRouter.navTo(sRouteName, {
+								context: sPath,
+								masterContext: sMasterContext
+							}, false);
+						}
+					}.bind(this));
+				}
+			} else {
+				this.oRouter.navTo(sRouteName);
+			}
+
+			if (typeof fnPromiseResolve === "function") {
+				fnPromiseResolve();
+			}
+		},
+		_onSearchFieldLiveChange: function(oEvent) {
+			var sControlId =
+				"sap_m_Page_0-content-sap_m_IconTabBar-1506348176785-items-sap_m_IconTabFilter-1-content-build_simple_Table-1506356963393";
+			var oControl = this.getView().byId(sControlId);
+
+			// Get the search query, regardless of the triggered event ('query' for the search event, 'newValue' for the liveChange one).
+			var sQuery = oEvent.getParameter("query") || oEvent.getParameter("newValue");
+			var sSourceId = oEvent.getSource().getId();
+
+			return new Promise(function(fnResolve) {
+				var aFinalFilters = [];
+
+				var aFilters = [];
+				// 1) Search filters (with OR)
+				if (sQuery && sQuery.length > 0) {
+
+					aFilters.push(new sap.ui.model.Filter("EBELN", sap.ui.model.FilterOperator.Contains, sQuery));
+
+					var iQuery = parseFloat(sQuery);
+					if (!isNaN(iQuery)) {
+						aFilters.push(new sap.ui.model.Filter("SHIP_NO", sap.ui.model.FilterOperator.EQ, sQuery));
+					}
+
+					aFilters.push(new sap.ui.model.Filter("DESCRIPTION", sap.ui.model.FilterOperator.Contains, sQuery));
+
+				}
+
+				var aFinalFilters = aFilters.length > 0 ? [new sap.ui.model.Filter(aFilters, false)] : [];
+				var oBindingOptions = this.updateBindingOptions(sControlId, {
+					filters: aFinalFilters
+				}, sSourceId);
+				var oBindingInfo = oControl.getBindingInfo("items");
+				oControl.bindAggregation("items", {
+					model: oBindingInfo.model,
+					path: oBindingInfo.path,
+					parameters: oBindingInfo.parameters,
+					template: oBindingInfo.template,
+					sorter: oBindingOptions.sorters,
+					filters: oBindingOptions.filters
+				});
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		updateBindingOptions: function(sCollectionId, oBindingData, sSourceId) {
+			this.mBindingOptions[sCollectionId] = this.mBindingOptions[sCollectionId] || {};
+
+			var aSorters = oBindingData.sorters === undefined ? this.mBindingOptions[sCollectionId].sorters : oBindingData.sorters;
+			var oGroupby = oBindingData.groupby === undefined ? this.mBindingOptions[sCollectionId].groupby : oBindingData.groupby;
+
+			// 1) Update the filters map for the given collection and source
+			this.mBindingOptions[sCollectionId].sorters = aSorters;
+			this.mBindingOptions[sCollectionId].groupby = oGroupby;
+			this.mBindingOptions[sCollectionId].filters = this.mBindingOptions[sCollectionId].filters || {};
+			this.mBindingOptions[sCollectionId].filters[sSourceId] = oBindingData.filters || [];
+
+			// 2) Reapply all the filters and sorters
+			var aFilters = [];
+			for (var key in this.mBindingOptions[sCollectionId].filters) {
+				aFilters = aFilters.concat(this.mBindingOptions[sCollectionId].filters[key]);
+			}
+
+			// Add the groupby first in the sorters array
+			if (oGroupby) {
+				aSorters = aSorters ? [oGroupby].concat(aSorters) : [oGroupby];
+			}
+
+			var aFinalFilters = aFilters.length > 0 ? [new sap.ui.model.Filter(aFilters, true)] : undefined;
+			return {
+				filters: aFinalFilters,
+				sorters: aSorters
+			};
+
+		},
+		_onButtonPress5: function(oEvent) {
+
+			var sDialogName = "Dialog1";
+			this.mDialogs = this.mDialogs || {};
+			var oDialog = this.mDialogs[sDialogName];
+			var oSource = oEvent.getSource();
+			var oBindingContext = oSource.getBindingContext();
+			var sPath = (oBindingContext) ? oBindingContext.getPath() : null;
+			var oView;
+			if (!oDialog) {
+				this.getOwnerComponent().runAsOwner(function() {
+					oView = sap.ui.xmlview({
+						viewName: "com.sap.build.standard.zmmtransport.view." + sDialogName
+					});
+					this.getView().addDependent(oView);
+					oView.getController().setRouter(this.oRouter);
+					oDialog = oView.getContent()[0];
+					this.mDialogs[sDialogName] = oDialog;
+				}.bind(this));
+			}
+
+			return new Promise(function(fnResolve) {
+				oDialog.attachEventOnce("afterOpen", null, fnResolve);
+				oDialog.open();
+				if (oView) {
+					oDialog.attachAfterOpen(function() {
+						oDialog.rerender();
+					});
+				} else {
+					oView = oDialog.getParent();
+				}
+
+				var oModel = this.getView().getModel();
+				if (oModel) {
+					oView.setModel(oModel);
+				}
+				if (sPath) {
+					var oParams = oView.getController().getBindingParameters();
+					oView.bindObject({
+						path: sPath,
+						parameters: oParams
+					});
+				}
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onTableItemPress1: function(oEvent) {
+
+			var oBindingContext = oEvent.getParameter("listItem").getBindingContext();
+
+			return new Promise(function(fnResolve) {
+				this.doNavigate("View_1", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onButtonPress6: function(oEvent) {
+
+			var sDialogName = "Dialog1";
+			this.mDialogs = this.mDialogs || {};
+			var oDialog = this.mDialogs[sDialogName];
+			var oSource = oEvent.getSource();
+			var oBindingContext = oSource.getBindingContext();
+			var sPath = (oBindingContext) ? oBindingContext.getPath() : null;
+			var oView;
+			if (!oDialog) {
+				this.getOwnerComponent().runAsOwner(function() {
+					oView = sap.ui.xmlview({
+						viewName: "com.sap.build.standard.zmmtransport.view." + sDialogName
+					});
+					this.getView().addDependent(oView);
+					oView.getController().setRouter(this.oRouter);
+					oDialog = oView.getContent()[0];
+					this.mDialogs[sDialogName] = oDialog;
+				}.bind(this));
+			}
+
+			return new Promise(function(fnResolve) {
+				oDialog.attachEventOnce("afterOpen", null, fnResolve);
+				oDialog.open();
+				if (oView) {
+					oDialog.attachAfterOpen(function() {
+						oDialog.rerender();
+					});
+				} else {
+					oView = oDialog.getParent();
+				}
+
+				var oModel = this.getView().getModel();
+				if (oModel) {
+					oView.setModel(oModel);
+				}
+				if (sPath) {
+					var oParams = oView.getController().getBindingParameters();
+					oView.bindObject({
+						path: sPath,
+						parameters: oParams
+					});
+				}
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		onInit: function() {
+
+			this.mBindingOptions = {};
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("Main").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+
+		}
+	});
+}, /* bExport= */ true);
